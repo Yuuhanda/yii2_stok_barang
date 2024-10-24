6430c8b442357301d1d8c8b04cbe57d75e7c3607
YuuHanda <55708987+Yuuhanda@users.noreply.github.com>
2024-10-24 13:59:26 +0700

    bug fixes

--- views/Item/_form.php
+++ views/Item/_form.php
@@ -10,11 +10,13 @@ use yii\widgets\ActiveForm;
 
 <div class="item-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
 
     <?= $form->field($model, 'item_name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'SKU')->textInput(['maxlength' => true]) ?>
+
+     <!-- <?//= $form->field($model, 'imageFile')->fileInput() ?> File input for image upload -->
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
