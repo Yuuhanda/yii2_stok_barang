a44fa3bd7c65c4c64051ab525b674f5124e1c970
YuuHanda <55708987+Yuuhanda@users.noreply.github.com>
2024-10-31 09:35:08 +0700

    function name changes to be more consistent

--- views/Lending/lending-list.php
+++ views/Lending/lending-list.php
@@ -17,8 +17,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-
-
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
     <?=  Html::a('Export Lending Data to XLSX', ['export/export-lending'], [
     'class' => 'btn btn-success',
